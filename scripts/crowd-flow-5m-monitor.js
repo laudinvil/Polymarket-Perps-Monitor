13e081d982739e2503c07d89e5a9c312b47cfebe
laudinvil <43842041+laudinvil@users.noreply.github.com>
2026-09-16 16:47:40 +0300

    Raise Crowd Flow trade alert threshold to 3000

--- scripts/crowd-flow-5m-monitor.js
+++ scripts/crowd-flow-5m-monitor.js
@@ -2,7 +2,7 @@ const { bucketStart, findMarketByEpoch } = require('../src/polymarket');
 const { sendTelegramMessage } = require('../src/telegram');
 
 const PERIOD = 300000;
-const ALERT_THRESHOLD = 2800;
+const ALERT_THRESHOLD = 3000;
 const LOW_TRADE_THRESHOLD = 800;
 const DATA_API = 'https://data-api.polymarket.com/trades';
 const saved = new Set();
