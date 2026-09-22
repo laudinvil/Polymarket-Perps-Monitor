c4f20361f97e15d2e36fae6ce66719d8fdfa3892
laudinvil <43842041+laudinvil@users.noreply.github.com>
2026-09-22 14:16:46 +0300

    Use resilient MarginPad symbol feed with live fallback

--- scripts/marginpad-btc-liquidation-5m.js
+++ scripts/marginpad-btc-liquidation-5m.js
@@ -1,4 +1,4 @@
-const { fetchLiveFeed, normalizeTs, normalizeSymbol, bucketStart } = require('../src/liquidation-monitor');
+const { fetchSymbolFeed, normalizeTs, normalizeSymbol, bucketStart } = require('../src/liquidation-monitor');
 const { findCurrentMarket, findClobMidpoint } = require('../src/polymarket');
 const { sendTelegramMessage } = require('../src/telegram');
 
@@ -167,7 +167,7 @@ async function main() {
       void heartbeatConvexRuntime();
       let events;
       try {
-        events = (await fetchLiveFeed()).filter(event => normalizeSymbol(event.symbol) === SYMBOL);
+        events = await fetchSymbolFeed(SYMBOL);
       } catch (error) {
         void logConvexRuntime('error', 'MarginPad poll error: ' + error.message);
         throw error;
